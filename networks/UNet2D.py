959d4d68fbf2ee9d1fedd58c6d3b5b80be7eb3a5
ThinhQuocLe007 <thinhquocle007@gmail.com>
2025-04-23 23:56:17 +0700

    Update MAE

--- networks/UNet2D.py
+++ networks/UNet2D.py
@@ -1,103 +1,113 @@
+# -*- coding: utf-8 -*-
+"""
+The implementation is borrowed from: https://github.com/HiLab-git/PyMIC
+"""
 from __future__ import division, print_function
 
-import numpy as np 
-import torch 
-import torch.nn as nn 
-from torch.nn import functional as F 
+import numpy as np
+import torch
+import torch.nn as nn
+import pdb
+from torch.nn import functional as F
 from torch.distributions.uniform import Uniform
 
-class ConvBlock(nn.Module): 
-    def __init__(self, in_channels, out_channels, dropout_p): 
-        super(ConvBlock, self).__init__() 
+
+class ConvBlock(nn.Module):
+    """two convolution layers with batch norm and leaky relu"""
+
+    def __init__(self, in_channels, out_channels, dropout_p):
+        super(ConvBlock, self).__init__()
         self.conv_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size= 3, padding= 1), 
-            nn.BatchNorm2d(out_channels), 
-            nn.LeakyReLU(), 
-            nn.Dropout(dropout_p), 
-            
-            nn.Conv2d(out_channels, out_channels, kernel_size= 3, padding= 1),
-            nn.BatchNorm2d(out_channels), 
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(),
+            nn.Dropout(dropout_p),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
             nn.LeakyReLU()
         )
-    
-    def forward(self, x): 
-        return self.conv_conv(x) 
-    
-class DownBlock(nn.Module): 
-    """
-    Downsampling followed by ConvBlock
-    """
-    def __init__(self, in_channels, out_channels, dropout_p): 
-        super(DownBlock, self).__init__() 
+
+    def forward(self, x):
+        return self.conv_conv(x)
+
+
+class DownBlock(nn.Module):
+    """Downsampling followed by ConvBlock"""
+
+    def __init__(self, in_channels, out_channels, dropout_p):
+        super(DownBlock, self).__init__()
         self.maxpool_conv = nn.Sequential(
-            nn.MaxPool2d(2), 
+            nn.MaxPool2d(2),
             ConvBlock(in_channels, out_channels, dropout_p)
         )
-    
-    def forward(self, x): 
-        return self.maxpool_conv(x)
-    
 
-class UpBlock(nn.Module): 
-    """
-    Upsampling followed by the ConvBlock
-    """
-    def __init__(self, in_channels1, in_channels2, out_channels, dropout_p): 
+    def forward(self, x):
+        return self.maxpool_conv(x)
+
+
+class UpBlock(nn.Module):
+    """Upsampling followed by ConvBlock"""
+
+    def __init__(self, in_channels1, in_channels2, out_channels, dropout_p):
         super(UpBlock, self).__init__()
-        self.convx1 = nn.Conv2d(in_channels1, in_channels2, kernel_size= 1) 
-        self.up = nn.Upsample(scale_factor= 2, mode= 'bilinear', align_corners= True) 
-        self.conv = ConvBlock(in_channels2 * 2, out_channels, dropout_p) 
+        self.conv1x1 = nn.Conv2d(in_channels1, in_channels2, kernel_size=1)
+        self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+        self.conv = ConvBlock(in_channels2 * 2, out_channels, dropout_p)
 
     def forward(self, x1, x2):
-        x1 = self.convx1(x1) 
-        x1 = self.up(x1) 
-        x = torch.cat([x2, x1], dim= 1) 
-        return self.conv(x)  
-    
-class Encoder(nn.Module): 
-    def __init__(self, params): 
-        super(Encoder, self).__init__() 
-        self.params = params 
+        x1 = self.conv1x1(x1)
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
+
+class Encoder(nn.Module):
+    def __init__(self, params):
+        super(Encoder, self).__init__()
+        self.params = params
         self.in_chns = self.params['in_chns']
         self.ft_chns = self.params['feature_chns']
         self.n_class = self.params['class_num']
         self.dropout = self.params['dropout']
-
         assert (len(self.ft_chns) == 5)
+        self.in_conv = ConvBlock(
+            self.in_chns, self.ft_chns[0], self.dropout[0])
+        self.down1 = DownBlock(
+            self.ft_chns[0], self.ft_chns[1], self.dropout[1])
+        self.down2 = DownBlock(
+            self.ft_chns[1], self.ft_chns[2], self.dropout[2])
+        self.down3 = DownBlock(
+            self.ft_chns[2], self.ft_chns[3], self.dropout[3])
+        self.down4 = DownBlock(
+            self.ft_chns[3], self.ft_chns[4], self.dropout[4])
 
-        self.in_conv = ConvBlock(self.in_chns,self.ft_chns[0], self.dropout[0])
-        self.down1 = DownBlock(self.ft_chns[0], self.ft_chns[1], self.dropout[1])
-        self.down2 = DownBlock(self.ft_chns[1], self.ft_chns[2], self.dropout[2])
-        self.down3 = DownBlock(self.ft_chns[2], self.ft_chns[3], self.dropout[3])
-        self.down4 = DownBlock(self.ft_chns[3], self.ft_chns[4], self.dropout[4])
-    
-    def forward(self, x): 
-        x0 = self.in_conv(x) 
+    def forward(self, x):
+        x0 = self.in_conv(x)
         x1 = self.down1(x0)
-        x2 = self.down2(x1) 
+        x2 = self.down2(x1)
         x3 = self.down3(x2)
-        x4 = self.down4(x3) 
+        x4 = self.down4(x3)
         return [x0, x1, x2, x3, x4]
-    
-class Decoder(nn.Module): 
-    def __init__(self, params): 
-        super(Decoder, self).__init__() 
-        self.params = params 
+
+
+class Decoder(nn.Module):
+    def __init__(self, params):
+        super(Decoder, self).__init__()
+        self.params = params
         self.in_chns = self.params['in_chns']
         self.ft_chns = self.params['feature_chns']
         self.n_class = self.params['class_num']
+        assert (len(self.ft_chns) == 5)
 
-        assert(len(self.ft_chns) == 5)
+        self.up1 = UpBlock(self.ft_chns[4], self.ft_chns[3], self.ft_chns[3], dropout_p=0.0)
+        self.up2 = UpBlock(self.ft_chns[3], self.ft_chns[2], self.ft_chns[2], dropout_p=0.0)
+        self.up3 = UpBlock(self.ft_chns[2], self.ft_chns[1], self.ft_chns[1], dropout_p=0.0)
+        self.up4 = UpBlock(self.ft_chns[1], self.ft_chns[0], self.ft_chns[0], dropout_p=0.0)
 
-        self.up1 = UpBlock(self.ft_chns[4], self.ft_chns[3], self.ft_chns[3], dropout_p= 0.0)
-        self.up2 = UpBlock(self.ft_chns[3], self.ft_chns[2], self.ft_chns[2], dropout_p= 0.0)
-        self.up3 = UpBlock(self.ft_chns[2], self.ft_chns[1], self.ft_chns[1], dropout_p= 0.0)
-        self.up4 = UpBlock(self.ft_chns[1], self.ft_chns[0], self.ft_chns[0], dropout_p= 0.0) 
+        self.out_conv = nn.Conv2d(self.ft_chns[0], self.n_class, kernel_size=3, padding=1)
 
-        self.conv = nn.Conv2d(self.ft_chns[0], self.n_class, kernel_size=3, padding=1) 
-    
-    def forward(self, feature): 
-        x0 = feature[0] 
+    def forward(self, feature):
+        x0 = feature[0]
         x1 = feature[1]
         x2 = feature[2]
         x3 = feature[3]
@@ -107,25 +117,85 @@ class Decoder(nn.Module):
         x = self.up2(x, x2)
         x = self.up3(x, x1)
         x_last = self.up4(x, x0)
-        output = self.conv(x_last) 
-        return output, x_last 
-    
-class UNet_2d(nn.Module): 
-    def __init__(self, in_chns, class_num): 
-        super(UNet_2d, self).__init__() 
+        output = self.out_conv(x_last)
+        return output, x_last
 
-        params = {
-            'in_chns': in_chns, 
-            'feature_chns': [16,32,64,128, 256], 
-            'dropout': [0.05, 0.1, 0.2, 0.3, 0.5], 
-            'class_num': class_num, 
-            'acti_func': 'relu'
-        }
+class UNet_2d(nn.Module):
+    def __init__(self, in_chns, class_num):
+        super(UNet_2d, self).__init__()
 
-        self.encoder = Encoder(params) 
-        self.decoder = Decoder(params) 
-    
-    def forward(self, x): 
-        feature = self.encoder(x) 
+        params = {'in_chns': in_chns,
+                  'feature_chns': [16, 32, 64, 128, 256],
+                  'dropout': [0.05, 0.1, 0.2, 0.3, 0.5],
+                  'class_num': class_num,
+                  'acti_func': 'relu'}
+
+        self.encoder = Encoder(params)
+        self.decoder = Decoder(params)
+        dim_in = 16
+        feat_dim = 32
+
+    def forward(self, x):
+        feature = self.encoder(x)
         output, features = self.decoder(feature)
-        return output 
+        return output
+
+class SharedDecoder(nn.Module): 
+    def __init__(self, feature_channels, out_channel): 
+        super(SharedDecoder, self).__init__() 
+        assert len(feature_channels) == 5 
+        self.up1 = UpBlock(feature_channels[4], feature_channels[3], feature_channels[3], dropout_p= 0.0)
+        self.up2 = UpBlock(feature_channels[3], feature_channels[2], feature_channels[2], dropout_p= 0.0)
+        self.up3 = UpBlock(feature_channels[2], feature_channels[1], feature_channels[1], dropout_p= 0.0)
+        self.up4 = UpBlock(feature_channels[1], feature_channels[0], feature_channels[0], dropout_p= 0.0)
+
+        self.out_conv = nn.Conv2d(feature_channels[0], out_channel, kernel_size= 3,padding= 1)
+
+    def forward(self, features): 
+        x0, x1, x2, x3, x4 = features 
+        x = self.up1(x4, x3) 
+        x = self.up2(x, x2)
+        x = self.up3(x, x1)
+        x_last = self.up4(x, x0)
+        output = self.out_conv(x_last)
+        return output, x_last 
+
+
+class DecoderSeg(SharedDecoder): 
+    def __init__(self): 
+        super().__init__(
+            feature_channels = [16,32,64,128,256], 
+            out_channel = 4 # acdc classes 
+        ) 
+
+class DecoderReconn(SharedDecoder): 
+    def __init__(self): 
+        super().__init__(
+            feature_channels= [16,32,64,128,256], 
+            out_channel= 1 # reconstruct the image 
+            
+        ) 
+
+class MBCP(nn.Module): 
+    def __init__(self, in_chns=1, class_num = 4): 
+        super(MBCP, self).__init__() 
+        params = {'in_chns': in_chns,
+                  'feature_chns': [16, 32, 64, 128, 256],
+                  'dropout': [0.05, 0.1, 0.2, 0.3, 0.5],
+                  'class_num': class_num,
+                  'acti_func': 'relu'}
+
+        self.encoder = Encoder(params)
+        self.decoder_seg = DecoderSeg() 
+        self.decoder_recon = DecoderReconn() 
+    def forward(self, x, mode='seg'):
+        features = self.encoder(x)
+        if mode == 'seg':
+            out, _ = self.decoder_seg(features)
+            return out
+        elif mode == 'recon':
+            out, _ = self.decoder_recon(features)
+            return out
+        else:
+            raise ValueError(f"Invalid mode: {mode}")
+         
