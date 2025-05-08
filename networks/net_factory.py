eef535bc830818fd649e19dafe6ca4b32f69e546
ThinhQuocLe007 <thinhquocle007@gmail.com>
2025-05-09 00:09:20 +0700

    Updated

--- networks/net_factory.py
+++ networks/net_factory.py
@@ -1,16 +1,16 @@
 from networks.UNet2D import UNet_2d, MBCP
-from networks.ResNet2d import ResUNet_2d
+from networks.ResNet2d import ResUNet_2d, ResUnetMAE
 import torch.nn as nn 
 
-def BCP_net(model='mbcp', in_chns=1, num_classes= 4, ema= False): 
-    if model == 'mbcp': 
+def BCP_net(model='MUnet', in_chns=1, num_classes= 4, ema= False): 
+    if model == 'MUnet': 
         net = MBCP(in_chns, num_classes).cuda()
         if ema: 
             for param in net.parameters(): 
                 param.detach_() 
         
-    elif model == 'ResUnet': 
-        net = ResUNet_2d(in_chns, num_classes).cuda()
+    elif model == 'MResUnet': 
+        net = ResUnetMAE(in_chns, num_classes).cuda()
         if ema: 
             for param in net.parameters(): 
                 param.detach_()
